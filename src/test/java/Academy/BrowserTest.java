a8350033d5a1df612c3ecf6d8b3d6a4ddb20bd68
rahulshetty <qaclickacademy@gmail.com>
2020-08-03 15:54:57 -0400

    sel test 1

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,6 +1,9 @@
 package Academy;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
 
@@ -9,5 +12,9 @@ public class BrowserTest {
 	public void getData()
 	{
 		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://rsdemo.azurewebsites.net/webapp/");
+		System.out.println(driver.findElement(By.cssSelector("h1")).getText());
 	}
 }
