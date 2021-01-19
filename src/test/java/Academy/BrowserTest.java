bc02f44839f3710d466b407c12e2fcc04b5cac81
sujaniw-spec <64097309+sujaniw-spec@users.noreply.github.com>
2021-01-19 16:38:00 -0700

    Update BrowserTest.java
    URL changed

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -19,7 +19,7 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		driver.get("http://rahulshettyacademydemo.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.cssSelector("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
